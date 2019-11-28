eee1093cae5f84739314866261ff99ac7a4eedcc
mae meier <maemeier@live.com>
2019-11-29 00:54:34 +0100

    animate

---
+++ src/design.js
@@ -0,0 +1,154 @@
+import React from "react";
+import Logo from "./img/LOGO.svg";
+import Logo1 from "./img/LOGO.svg";
+import Iphone from "./img/iphone.png";
+import ThaiAir from "./img/thaiAir.png";
+import Skyscanner from "./img/skyscanner.png";
+import Airbnb from "./img/airbnb.png";
+import TeeShop from "./img/teeShop.png";
+import "./design.css";
+
+const Homepage = () => {
+  return (
+    <div className="container">
+      <div className="ui secondary menu">
+        <img className="Logo" src={Logo} alt="logo" />
+        <a className="item right">Home</a>
+        <a className="item ">Projects</a>
+        <a className="item left">Design</a>
+      </div>
+      <div className="header">
+        <svg
+          className="svg1"
+          width="1439"
+          height="474"
+          viewBox="0 0 1439 474"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M1439 0H0V474L1439 175.156V0Z" fill="#E8BCBC" />
+          <path d="M1439 0H0V474L1439 175.156V0Z" fill="url(#paint0_linear)" />
+          <defs>
+            <linearGradient
+              id="paint0_linear"
+              x1="719"
+              y1="-104.18"
+              x2="718.787"
+              y2="689"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stop-color="#A26E6E" />
+              <stop offset="1" stop-color="white" stop-opacity="0" />
+            </linearGradient>
+          </defs>
+        </svg>
+      </div>
+
+      <div className="body">
+        <img className="iphone floating" src={Iphone} alt="logo" />
+        <div>
+          <h1 className="iphoneHeader">
+            Design won't save the world, but it makes your apps look good
+          </h1>
+          <p className="iphoneText">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+      </div>
+
+      <div className="body2">
+        <div>
+          <h1 className="thaiAirHeader">REDESIGN THAI AIRWAY</h1>
+          <p className="thaiAirText">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
+          </p>
+        </div>
+        <img className="thaiAir" src={ThaiAir} alt="logo" />
+      </div>
+
+      <h1 className="mobileHeader">MOBILE APP</h1>
+      <p className="mobileText">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur.
+      </p>
+
+      <div className="allApp">
+        <img className="appImage sky" src={Skyscanner} alt="logo" />
+        <img className="appImage air" src={Airbnb} alt="logo" />
+        <img className="appImage green" src={TeeShop} alt="logo" />
+      </div>
+      <div className="footer">
+        <div className="svgBlock">
+          <svg
+            className="svg2"
+            width="1443"
+            height="448"
+            viewBox="0 0 1443 448"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M1442 447H1V1L1442 210.882V447Z"
+              fill="#242424"
+              stroke="black"
+            />
+          </svg>
+        </div>
+
+        <div className="contackBlock">
+          <p className="contact"> CONTACT</p>
+          <div className="info">
+            <div>
+              {" "}
+              <strong>Mae</strong> Kangsadan
+            </div>
+            <div>
+              {" "}
+              <a className="footerLink" href="https://www.github/maemeier">
+                {" "}
+                <strong>Github:</strong> maemeier
+              </a>
+            </div>
+            <div>
+              {" "}
+              <a
+                className="footerLink"
+                href="https://www.pinterest.com/fynista/"
+              >
+                <strong>Pinterest:</strong> fynista
+              </a>
+            </div>
+          </div>
+          <div className="info2">
+            <div>
+              <strong>Email: </strong>info@fyni.de
+            </div>
+            <div>
+              <strong> Linkdel: </strong>maemeier
+            </div>
+            <div>
+              {" "}
+              <strong>Instagram: </strong>fynista
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
---
+++ src/homepage.js
@@ -0,0 +1,179 @@
+import React from "react";
+
+import logo from "./logo.svg";
+import affinity from "./img/affinity.png";
+import database from "./img/dataBaseLogo.png";
+import css3 from "./img/css3logo.png";
+import jsLogo from "./img/jsLogo.png";
+import reactLogo from "./img/reactLogo.png";
+import html from "./img/htmlLogo.png";
+import fyni from "./img/fyniLogo.png";
+import fullstackLogo from "./img/fullstack.png";
+import designLogo2 from "./img/designLogo2.png";
+import Logo1 from "./img/LOGO.svg";
+import "./landing.css";
+
+const Homepage = () => {
+  return (
+    <div className="container">
+      <div class="ui secondary   menu">
+        <a class="item">Home</a>
+        <a class="item">Projects</a>
+        <a class="item">Design</a>
+      </div>
+      <div className="header">
+        <svg
+          className="svg1"
+          width="1439"
+          height="474"
+          viewBox="0 0 1439 474"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M1439 0H0V474L1439 175.156V0Z" fill="#E8BCBC" />
+          <path d="M1439 0H0V474L1439 175.156V0Z" fill="url(#paint0_linear)" />
+          <defs>
+            <linearGradient
+              id="paint0_linear"
+              x1="719"
+              y1="-104.18"
+              x2="718.787"
+              y2="689"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stop-color="#A26E6E" />
+              <stop offset="1" stop-color="white" stop-opacity="0" />
+            </linearGradient>
+          </defs>
+        </svg>
+      </div>
+
+      <div>
+        <img className="logo" src={Logo1} alt="logo" />
+      </div>
+
+      <p className="goldfish"> DESIGN AND CODE</p>
+      <div className="underLineText"></div>
+
+      <div className="listLogo">
+        <img className="logo1" src={html} alt="logo" />
+        <img className="logo1" src={jsLogo} alt="logo" />
+        <img className="logo1" src={css3} alt="logo" />
+        <img className="logo1" src={reactLogo} alt="logo" />
+        <img className="logo1" src={database} alt="logo" />
+        <img className="logo1" src={affinity} alt="logo" />
+      </div>
+
+      <div class="textBlock">
+        <p class="firstBlock">
+          <strong>
+            Hello World! Welcome to FYNI.DEV, My name is Mae, Junior Front-end
+            Developer --
+          </strong>
+        </p>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+
+      <p class="projects">
+        {" "}
+        <span>PROJECTS </span>
+      </p>
+
+      <div class="allProjects">
+        <div>
+          <img class="fullstackLogo" src={fullstackLogo} alt="logo" />
+        </div>
+        <div>
+          <img class="designLogo" src={designLogo2} alt="logo" />
+        </div>
+        <div>
+          <p class="projectText">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.{" "}
+          </p>
+        </div>
+      </div>
+
+      <div className="footer">
+        <div className="svgBlock">
+          <svg
+            className="svg2"
+            width="1443"
+            height="448"
+            viewBox="0 0 1443 448"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M1442 447H1V1L1442 210.882V447Z"
+              fill="#242424"
+              stroke="black"
+            />
+          </svg>
+        </div>
+
+        <div className="contackBlock">
+          <p className="contact"> CONTACT</p>
+          <div className="info">
+            <div>
+              {" "}
+              <strong>Mae</strong> Kangsadan
+            </div>
+            <div>
+              {" "}
+              <a className="footerLink" href="https://www.github/maemeier">
+                {" "}
+                <strong>Github:</strong> maemeier
+              </a>
+            </div>
+            <div>
+              {" "}
+              <a
+                className="footerLink"
+                href="https://www.pinterest.com/fynista/"
+              >
+                <strong>Pinterest:</strong> fynista
+              </a>
+            </div>
+          </div>
+          <div className="info2">
+            <div>
+              <strong>Email: </strong>info@fyni.de
+            </div>
+            <div>
+              <strong> Linkdel: </strong>maemeier
+            </div>
+            <div>
+              {" "}
+              <strong>Instagram: </strong>fynista
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
