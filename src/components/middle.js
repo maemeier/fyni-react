6963dda99338752ab1d201e5db6282a58d6cd522
Mae Kangsadan <maemeier@live.com>
2019-12-28 21:20:47 +0100

    add icon

--- src/components/middle.js
+++ src/components/middle.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ThaiAir from "../img/thaiAir.png";
 import "../homepage.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { Link } from "react-router-dom";
+
+const icon = (
+  <FontAwesomeIcon size="2x" color="var(--mainBlack)" icon={faGithub} />
+);
 
 const Middle = () => {
   return (
@@ -20,7 +27,11 @@ const Middle = () => {
           problem sloving skills. So if you are looking for Front-end developer
           who loves to use react and capable of spending whole day fixing bugs,
           just scroll down a little bit to the end of the page till you find my
-          contact! Hope to hear from you soon.--
+          contact! Hope to hear from you soon.-- Check my GitHub <br />
+          <br />
+          <a className="link" href="https://github.com/maemeier">
+            {icon}
+          </a>
         </p>
       </div>
     </div>
